e9470fb739c03495923d4c821a2b461e59741f28
Aditya Gupta <adityagupta1807@gmail.com>
2021-01-11 13:54:20 +0530

    change

--- G_Coding _interview.py
+++ G_Coding _interview.py
@@ -1,9 +1,11 @@
 #google coding interviw question 1 
 def converttonum(word):
-    dict1={}
+    dict1={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'} #to fill with string
     string=''
     for i in word:
-        string.append(dict1[i])
+        for k,v in dict1.items():
+            if i in v:
+                string+=k
     return string
 def possiblestrings(phoneno,listofstrings):
     # 1st approach would be to map the string to numbers and find it in the phone number
@@ -12,7 +14,13 @@ def possiblestrings(phoneno,listofstrings):
     possiblestrings1=[]
     for num,word in listpair:
         if num in phoneno:
-            possiblestrings.append(word)
+            possiblestrings1.append(word)
     return possiblestrings1
 
-print(possiblestrings('1800-356-9377',['flowers']))
+phone=input('enter phone no.: ')
+phone=phone.replace('-','')
+x=int(input('enter no of test words: '))
+lst=[]
+for i in range(x):
+    lst.append(input(f'enter  word {i+1}: '))
+    
